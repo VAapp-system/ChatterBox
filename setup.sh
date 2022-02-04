e14963907f3ac94425398ac5aae7a92625c0f112
unknown <service.gassistant@gmail.com>
2022-02-05 00:57:26 +0900

    Set Up Tool

---
+++ setup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+python3 setup/category_extract.py $1
+python3 setup/app_extract.py $1
+python3 setup/page.py $1
+python3 setup/command.py $1
+python3 setup/name.py $1
+python3 setup/description.py $1
+python3 setup/NEextract.py $1
+
+mkdir debug
+mkdir logfile
+mkdir error
+mkdir user_storage
+mkdir canvas
+mkdir tree
