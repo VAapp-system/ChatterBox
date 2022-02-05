5df1374a865665d905084c0172c5862907114fa2
unknown <service.gassistant@gmail.com>
2022-02-05 12:27:01 +0900

    Install Tool

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sed -i -e "s/your_gmail/$1/g" ./ChatterBox.py ./ChatterBox.py
-sed -i -e "s/your_gmail/$1/g" ./reset/Reset_Login.py ./reset/Reset_Login.py
-sed -i -e "s/your_gmail/$1/g" ./reset/Reset_Simulator.py ./reset/Reset_Simulator.py
+sed -i -e "s/your_gmail/$1/g" ./ChatterBox_template.py ./ChatterBox.py
+sed -i -e "s/your_gmail/$1/g" ./reset/Reset_Login_template.py ./reset/Reset_Login.py
+sed -i -e "s/your_gmail/$1/g" ./reset/Reset_Simulator_template.py ./reset/Reset_Simulator.py
 
-sed -i -e "s/your_password/$2/g" ./ChatterBox.py ./ChatterBox.py
-sed -i -e "s/your_password/$2/g" ./reset/Reset_Login.py ./reset/Reset_Login.py
-sed -i -e "s/your_password/$2/g" ./reset/Reset_Simulator.py ./reset/Reset_Simulator.py
+sed -i -e "s/your_password/$2/g" ./ChatterBox_template.py ./ChatterBox.py
+sed -i -e "s/your_password/$2/g" ./reset/Reset_Login_template.py ./reset/Reset_Login.py
+sed -i -e "s/your_password/$2/g" ./reset/Reset_Simulator_template.py ./reset/Reset_Simulator.py
