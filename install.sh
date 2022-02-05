6195852ce3f4af88e924804e5eeb58406dfc90fe
unknown <service.gassistant@gmail.com>
2022-02-05 17:56:49 +0900

    Install Tool

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-sed -i -e "s/your_gmail/$1/g" ./ChatterBox_template.py ./ChatterBox.py
-sed -i -e "s/your_gmail/$1/g" ./reset/Reset_Login_template.py ./reset/Reset_Login.py
-sed -i -e "s/your_gmail/$1/g" ./reset/Reset_Simulator_template.py ./reset/Reset_Simulator.py
-
-sed -i -e "s/your_password/$2/g" ./ChatterBox_template.py ./ChatterBox.py
-sed -i -e "s/your_password/$2/g" ./reset/Reset_Login_template.py ./reset/Reset_Login.py
-sed -i -e "s/your_password/$2/g" ./reset/Reset_Simulator_template.py ./reset/Reset_Simulator.py
+sed -e "s/your_gmail/$1/g" -e "s/your_password/$2/g" ./ChatterBox_template.py > ./ChatterBox.py
+sed -e "s/your_gmail/$1/g" -e "s/your_password/$2/g" ./reset/Reset_Login_template.py > ./reset/Reset_Login.py
+sed -e "s/your_gmail/$1/g" -e "s/your_password/$2/g" ./reset/Reset_Simulator_template.py > ./reset/Reset_Simulator.py
