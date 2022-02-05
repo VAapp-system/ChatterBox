a317d4af6a39b9889b53cfec0fedb401f188c96c
unknown <service.gassistant@gmail.com>
2022-02-06 03:13:46 +0900

    Install Tool

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ sed -e "s/your_gmail/$1/g" -e "s/your_password/$2/g" ./ChatterBox_template.py > 
 sed -e "s/your_gmail/$1/g" -e "s/your_password/$2/g" ./reset/Reset_Login_template.py > ./reset/Reset_Login.py
 sed -e "s/your_gmail/$1/g" -e "s/your_password/$2/g" -e "s/your_project_name/$3/g" ./reset/Reset_Simulator_template.py > ./reset/Reset_Simulator.py
 sed -e "s/your_project_name/$3/g" ./module/simulator_template.py > ./module/simulator.py
-sed -e "s/Stanford_CoreNLP_server_URL/$4/g" ./module/questionEN_template.py > ./module/questionEN.py
+sed -e "s/nlp_url/$4/g" ./module/questionEN_template.py > ./module/questionEN.py
